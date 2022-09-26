428ee4d153d14434b14e8c2c19fc82a0565c608a
root <root@YogaSlim>
2022-09-27 00:15:22 +0700

    Bundle

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+5.times do
+  Location.create device_code: nil, latitude: 17.324363, longitude: 100.324363, receiving_time: DateTime.now, status: :yes
+end
